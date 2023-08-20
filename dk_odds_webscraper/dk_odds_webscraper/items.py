3e9c92e5a0c3e05e7b6086f96dcef7dbb93c612b
dhutchings1998 <dhutchings1998@gmail.com>
2023-08-20 15:01:57 -0400

    Adding additional fields to mlb game item

--- dk_odds_webscraper/dk_odds_webscraper/items.py
+++ dk_odds_webscraper/dk_odds_webscraper/items.py
@@ -1,12 +1,18 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
-class DkOddsWebscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class MLBGameItem(scrapy.Item):
+    team1_name = scrapy.Field()
+    team2_name = scrapy.Field()
+    event_time = scrapy.Field()
+    event_date = scrapy.Field()
+    last_updated_utc = scrapy.Field()
+    team1_point_spread = scrapy.Field()
+    team2_point_spread = scrapy.Field()
+    team1_total_points_over_under = scrapy.Field()
+    team2_total_points_over_under = scrapy.Field()
+    team1_money_line = scrapy.Field()
+    team2_money_line = scrapy.Field()
+    live = scrapy.Field()
+    inning = scrapy.Field()
+    team1_score = scrapy.Field()
+    team2_score = scrapy.Field()
--- dk_odds_webscraper/dk_odds_webscraper/spiders/mlb_odds_spider.py
+++ dk_odds_webscraper/dk_odds_webscraper/spiders/mlb_odds_spider.py
@@ -1,11 +1,13 @@
 import scrapy 
+import time
+from dk_odds_webscraper.items import MLBGameItem
 
 class MLBOddsSpider(scrapy.Spider):
     name = 'mlboddsspider'
     start_urls = ['https://sportsbook.draftkings.com/leagues/baseball/mlb?category=game-lines&subcategory=game']
 
     def parse(self, response):
-        
+
         table_categories = response.css('table.sportsbook-table')
 
         for category in table_categories:
@@ -18,50 +20,47 @@ class MLBOddsSpider(scrapy.Spider):
                 row1 = category.css(f'tbody > tr:nth-child({i})')
                 row2 = category.css(f'tbody > tr:nth-child({i+1})')
 
-                event_time = row1.css('th span.event-cell__start-time::text').get()
+                mlb_game_item = MLBGameItem()
 
-                team1_name = row1.css('div.event-cell__name-text::text').get()
-                team2_name = row2.css('div.event-cell__name-text::text').get()
+                mlb_game_item['team1_name'] = row1.css('div.event-cell__name-text::text').get()
+                mlb_game_item['team2_name'] = row2.css('div.event-cell__name-text::text').get()
+                
+                mlb_game_item['event_date'] = event_date
+                mlb_game_item['last_updated_utc'] = int(time.time())
 
-                team1_run_line = row1.css('td:nth-child(2) span.sportsbook-outcome-cell__line::text').get()
-                team1_run_line_odds = row1.css('td:nth-child(2) span.sportsbook-odds.american::text').get()
-
-                team2_run_line = row2.css('td:nth-child(2) span.sportsbook-outcome-cell__line::text').get()
-                team2_run_line_odds = row2.css('td:nth-child(2) span.sportsbook-odds.american::text').get()
-
-                team1_total_points = row1.css('td:nth-child(3) div.sportsbook-outcome-cell__label-line-container span::text').extract()
-                team1_total_points_odds = row1.css('td:nth-child(3) span.sportsbook-odds.american::text').get()
-
-                team2_total_points = row2.css('td:nth-child(3) div.sportsbook-outcome-cell__label-line-container span::text').extract()
-                team2_total_points_odds = row2.css('td:nth-child(3) span.sportsbook-odds.american::text').get()
-
-                team1_money_line = row1.css('td:nth-child(4) span.sportsbook-odds.american::text').get()
-                team2_money_line = row2.css('td:nth-child(4) span.sportsbook-odds.american::text').get()
-
-                yield {
-                    'team1': team1_name,
-                    'team2': team2_name,
-                    'event_time': event_time,
-                    'event_date': event_date,
-                    'team1_run_line': {
-                        'run_line': team1_run_line,
-                        'odds': team1_run_line_odds
-                    },
-                    'team2_run_line': {
-                        'run_line': team2_run_line,
-                        'odds': team2_run_line_odds
-                    },
-                    'team1_total_points': {
-                        'total_points': team1_total_points,
-                        'odds': team1_total_points_odds
-                    },
-                    'team2_total_points': {
-                        'total_points': team2_total_points,
-                        'odds': team2_total_points_odds
-                    },
-                    'team1_money_line': team1_money_line,
-                    'team2_money_line': team2_money_line,
+                mlb_game_item['team1_point_spread'] = {
+                    'line': row1.css('td:nth-child(2) span.sportsbook-outcome-cell__line::text').get(),
+                    'odds': row1.css('td:nth-child(2) span.sportsbook-odds.american::text').get()
+                }
+                mlb_game_item['team2_point_spread'] = {
+                    'line': row2.css('td:nth-child(2) span.sportsbook-outcome-cell__line::text').get(),
+                    'odds': row2.css('td:nth-child(2) span.sportsbook-odds.american::text').get()
                 }
 
+                mlb_game_item['team1_total_points_over_under'] = {
+                    'line': row1.css('td:nth-child(3) div.sportsbook-outcome-cell__label-line-container span::text').extract(),
+                    'odds': row1.css('td:nth-child(3) span.sportsbook-odds.american::text').get()
+                }
+                mlb_game_item['team2_total_points_over_under'] = {
+                    'line': row2.css('td:nth-child(3) div.sportsbook-outcome-cell__label-line-container span::text').extract(),
+                    'odds': row2.css('td:nth-child(3) span.sportsbook-odds.american::text').get()
+                }
 
+                mlb_game_item['team1_money_line'] = row1.css('td:nth-child(4) span.sportsbook-odds.american::text').get()
+                mlb_game_item['team2_money_line'] = row2.css('td:nth-child(4) span.sportsbook-odds.american::text').get()
 
+                event_time = row1.css('th span.event-cell__start-time::text').get()
+                if event_time: # event is not live
+                    mlb_game_item['event_time'] = event_time
+                    mlb_game_item['live'] = False
+                    mlb_game_item['inning'] = None
+                    mlb_game_item['team1_score'] = 0
+                    mlb_game_item['team2_score'] = 0
+                else: # event is live
+                    mlb_game_item['event_time'] = None
+                    mlb_game_item['live'] = True
+                    mlb_game_item['inning'] = row1.css('span.event-cell__period::text').get()
+                    mlb_game_item['team1_score'] = row1.css('span.event-cell__score::text').get()
+                    mlb_game_item['team2_score'] = row2.css('span.event-cell__score::text').get()
+
+                yield mlb_game_item
